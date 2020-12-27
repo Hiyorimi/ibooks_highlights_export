2fdb6a8ddee4fe1f76c6406be9c2e303cafcfe05
Hiyorimi <playittodeath@gmail.com>
2020-12-27 14:43:57 +0300

    improved codestyle

--- ibooks_highlights_exporter.py
+++ ibooks_highlights_exporter.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from typing import List, Dict
+from typing import List, Tuple, Union, Any, Dict
 import os
 import datetime
 from glob import glob
@@ -76,10 +76,12 @@ def get_chapters(ibooks_cursor, book_id: str) -> List[str]:
     return chapters
 
 
-def get_mind_map_contents(book_id: str):
-    """Loads mind map raw data for book_id."""
-    chapters = get_chapters(ibooks_cursor, book_id)
-    print(chapters)
+def get_chapters_and_annotations(
+        ibooks_cursor,
+        book_id: str,
+        chapters: List[str],
+) -> Tuple[List[List[Union[str,int]]], List[List[Union[str, Any]]]]:
+    """Returns list of book's chapters."""
 
     chapters_list = []
     counter = 1
@@ -111,27 +113,22 @@ def get_mind_map_contents(book_id: str):
         )
         counter += 1
 
-    # beginning another way of doing the same thing, just more efficient
-    res2 = assets_cursor.execute(
-        "select distinct(ZASSETID), ZTITLE, ZAUTHOR from ZBKLIBRARYASSET")
-    for assetid, title, author in res2:
-        asset_title_tab[assetid] = [assetid, title, author]
+    return chapters_list, annotations
 
-    today = datetime.date.isoformat(datetime.date.today())
-    print(get_book_details(book_id))
-    print(book_id)
 
-    template = TEMPLATE_ENVIRONMENT.get_template("open_mindmap.xml")
-
-    template.globals['get_mm_color'] = get_mm_color
-    template.globals['make_text_readable'] = make_text_readable
-    template.globals['get_book_details'] = get_book_details
-
+def create_nodes_for_open_mindmap(
+    chapters_list: List[List[Union[str,int]]],
+    annotations: List[List[Union[str, Any]]],
+) -> List[List[str, int, list]]:
+    """Assembles nodes for open mindmap format."""
     # Move annotations to chapter object
     chapters = {}
     nodes = []
     annotations_counter = 0
     fake_chapter_counter = 1
+    annotations_per_fake_chapter = 0
+
+    # Checking if we are able to get chapters from book
     if len(chapters_list) != 1:
         for ch in chapters_list:
             chapters[ch[0]] = []
@@ -148,7 +145,8 @@ def get_mind_map_contents(book_id: str):
         if len(chapters_list) != 1:
             chapters[ann[3]].append(content)
         else:
-            if (annotations_counter < annotations_per_fake_chapter):
+            # If we don't have chapters
+            if annotations_counter < annotations_per_fake_chapter:
                 fake_chapter_name = "Part " + str(fake_chapter_counter)
                 chapters[fake_chapter_name].append(content)
                 annotations_counter += 1
@@ -169,14 +167,49 @@ def get_mind_map_contents(book_id: str):
         except (TypeError, NameError) as e:
             print("error", k, len(chapters[k]), e)
 
+    return nodes
+
+
+def render_open_mindmap_xml(
+    book_id: str,
+    chapters_list: List[List[Union[str,int]]],
+    annotations: List[List[Union[str, Any]]],
+) -> str:
+    """Renders xml mind map."""
+    today = datetime.date.isoformat(datetime.date.today())
+    template = TEMPLATE_ENVIRONMENT.get_template("open_mindmap.xml")
+
+    template.globals['get_mm_color'] = get_mm_color
+    template.globals['make_text_readable'] = make_text_readable
+    template.globals['get_book_details'] = get_book_details
+
+    nodes = create_nodes_for_open_mindmap(chapters_list, annotations)
+
     smmx = template.render(
         obj={
             "last": "###",
             "date": today,
-            "assetlist": asset_title_tab,
             "book_name": get_book_details(book_id),
             "chapters": nodes
         })
+
+    return smmx
+
+
+def get_mind_map_contents(book_id: str):
+    """Loads mind map raw data for book_id."""
+    chapters = get_chapters(ibooks_cursor, book_id)
+    print(chapters)
+
+    chapters_list, annotations = get_chapters_and_annotations(
+        ibooks_cursor,
+        book_id,
+        chapters,
+    )
+    print(get_book_details(book_id))
+    print(book_id)
+
+    smmx = render_open_mindmap_xml(book_id, chapters_list, annotations)
 
     return smmx.encode('utf-8')
 
